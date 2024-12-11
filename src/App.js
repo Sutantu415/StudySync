68bb3b615d9af802c28341edbe7af83203ccdc54
brandonwong10 <119823011+brandonwong10@users.noreply.github.com>
2024-12-11 14:05:15 -0800

    Repo Cleanup

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import RoomPage from "./pages/roomPage";
 import RegisterPage from "./pages/register";
 import SessionPage from "./pages/session";
 import "./App.css";
-import TimerPage from "./pages/timer/timer";
 import AccountPage from "./pages/account/account";
-import SettingsPage from "./components/timerComponents/Settings";
 import { useState } from "react";
-import SettingsContext from "./components/timerComponents/SettingsContext";
 
 
 //function app for routes
 function App() {
-  const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(25);
-  const [breakMinutes, setBreakMinutes] = useState(5);
-  const [longBreakMinutes, setLongBreakMinutes] = useState(60);
-
   return (
     <Router>
       <Routes>
@@ -27,25 +19,6 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/session/:roomId" element={<SessionPage />} />
-        <Route
-          path="/timer"
-          element={
-            <SettingsContext.Provider
-              value={{
-                showSettings,
-                setShowSettings,
-                workMinutes,
-                breakMinutes,
-                longBreakMinutes,
-                setWorkMinutes,
-                setBreakMinutes,
-                setLongBreakMinutes,
-              }}
-            >
-              {showSettings ? <SettingsPage /> : <TimerPage />}
-            </SettingsContext.Provider>
-          }
-        />
       </Routes>
     </Router>
   );
